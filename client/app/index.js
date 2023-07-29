aaccd0b8b536557d155fce6d4fd16691257b212f
jameshuang98 <jameshuang337@gmail.com>
2023-07-28 22:12:25 -0700

    add scheduled tasks

--- client/app/index.js
+++ client/app/index.js
@@ -1,4 +1,5 @@
 import { View, Text, ScrollView, SafeAreaView } from 'react-native';
+import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
@@ -11,57 +12,68 @@ import { Ionicons } from '@expo/vector-icons';
 const Tab = createBottomTabNavigator();
 
 function App() {
+    useEffect(() => {
+        const intervalId = BackgroundFetch.registerTaskAsync(CREATE_RECORDS, {
+            minimumInterval: 24 * 60, // In minutes, every 24 hours (midnight).
+            stopOnTerminate: false,   // Continue running task even if the app is terminated.
+            startOnBoot: true,        // Start task when the device reboots.
+        });
+        return () => {
+            // Unregister the task when the component unmounts (optional).
+            BackgroundFetch.unregisterTaskAsync(intervalId);
+        };
+    }, []);
     return (
-            <NavigationContainer independent={true} screenOptions={{headerTitle:""}}>
-                <Tab.Navigator screenOptions={{ headerShown: false }}>
-                    <Tab.Screen
-                        name="Home"
-                        component={Home}
-                        
-                        options={{
-                            headerTitle:"",
-                            headerShown:false,
-                            headerShadowVisible: false,
-                            tabBarIcon: ({ focused, color, size }) => (
-                                <Ionicons
-                                    name={focused ? 'ios-home' : 'ios-home-outline'}
-                                    size={size}
-                                    color={color}
-                                />
-                            ),
-                        }}
-                    />
+        <NavigationContainer independent={true} screenOptions={{ headerTitle: "" }}>
+            <Tab.Navigator screenOptions={{ headerShown: false }}>
+                <Tab.Screen
+                    name="Home"
+                    component={Home}
 
-                    <Tab.Screen
-                        name="Prescription List"
-                        component={List}
-                        options={{
-                            tabBarIcon: ({ focused, color, size }) => (
-                                <Ionicons
-                                    name={focused ? 'list' : 'list-outline'}
-                                    size={size}
-                                    color={color}
-                                />
-                            ),
-                        }}
-                    />
+                    options={{
+                        headerTitle: "",
+                        headerShown: false,
+                        headerShadowVisible: false,
+                        tabBarIcon: ({ focused, color, size }) => (
+                            <Ionicons
+                                name={focused ? 'ios-home' : 'ios-home-outline'}
+                                size={size}
+                                color={color}
+                            />
+                        ),
+                    }}
+                />
 
-                    <Tab.Screen
-                        name="Calendar View"
-                        component={Calendar}
-                        options={{
-                            tabBarIcon: ({ focused, color, size }) => (
-                                <Ionicons
-                                    name={focused ? 'calendar' : 'calendar-outline'}
-                                    size={size}
-                                    color={color}
-                                />
-                            ),
-                        }}
-                    />
+                <Tab.Screen
+                    name="Prescription List"
+                    component={List}
+                    options={{
+                        tabBarIcon: ({ focused, color, size }) => (
+                            <Ionicons
+                                name={focused ? 'list' : 'list-outline'}
+                                size={size}
+                                color={color}
+                            />
+                        ),
+                    }}
+                />
 
-                </Tab.Navigator>
-            </NavigationContainer>
+                <Tab.Screen
+                    name="Calendar View"
+                    component={Calendar}
+                    options={{
+                        tabBarIcon: ({ focused, color, size }) => (
+                            <Ionicons
+                                name={focused ? 'calendar' : 'calendar-outline'}
+                                size={size}
+                                color={color}
+                            />
+                        ),
+                    }}
+                />
+
+            </Tab.Navigator>
+        </NavigationContainer>
     );
 }
 export default App;
