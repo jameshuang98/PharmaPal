5ddab719fd6d7dba82e11517cc75e2d541cf6454
jameshuang98 <jameshuang337@gmail.com>
2023-08-11 22:43:22 -0700

    add react-native-picker and create datapicker component

---
+++ client/components/common/DataPicker.jsx
@@ -0,0 +1,50 @@
+import { View, Text, StyleSheet } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { useState } from 'react';
+
+const DataPicker = (props) => {
+    const { label, values } = props;
+    const [selectedLanguage, setSelectedLanguage] = useState();
+
+    return (
+        <View>
+            <Text style={styles.label}>{label}</Text>
+            <Picker
+                selectedValue={selectedLanguage}
+                onValueChange={(itemValue, itemIndex) =>
+                    setSelectedLanguage(itemValue)
+                }
+                style={styles.input}
+            >
+                {values.map((item, index) => (
+                    <Picker.Item label={item} value={item} key={index} />
+                ))}
+            </Picker>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    input: {
+        borderWidth: 1,
+        borderColor: '#C0C0C0',
+        borderRadius: 8,
+        paddingVertical: 12,
+        paddingHorizontal: 16,
+        fontSize: 16,
+        color: '#000',
+        backgroundColor: '#F0F0F0',
+        marginBottom: 10,
+    },
+    inputFocused: {
+        borderColor: 'black',
+    },
+    label: {
+        fontSize: 16,
+        fontWeight: 'bold',
+        color: '#333',
+        marginBottom: 2,
+    }
+});
+
+export default DataPicker;
