fc82cf9214d79b651c99163f2a0ffe2d214982d9
jameshuang98 <jameshuang337@gmail.com>
2023-08-25 22:16:16 -0700

    use onsnapshot to get prescription data in real time

--- client/app/hooks/usePrescriptionData.js
+++ client/app/hooks/usePrescriptionData.js
@@ -45,7 +45,6 @@ export default function usePrescriptionData() {
     const recordRef = collection(db, 'record');
 
 
-
     // state.prescriptionData
     // [{"id": "123123sldflkj", "userId":"1", "title":"Zoloft", "createdAt":"123123123", "dailyFrequency":"2", "frequency":"1", "json":"{"1": "1690167371", "2": "1690167372" }", "status":"active"}]
 
@@ -58,29 +57,75 @@ export default function usePrescriptionData() {
 
 
     // getting data and storing it in state
+    // useEffect(() => {
+    //     let prescriptions = []
+    //     getDocs(prescriptionRef)
+    //         .then((snapshot) => {
+    //             snapshot.docs.forEach((doc) => {
+    //                 prescriptions.push({ ...doc.data(), id: doc.id })
+    //             })
+    //             setState({
+    //                 prescriptionData: prescriptions
+    //             });
+    //         })
+    //         .catch(err => {
+    //             console.log(err.message)
+    //         });
+    // }, []);
+
+
+    // subscribe to the prescription collection to get real time collection data
+    // onSnapshot(prescriptionRef, (snapshot) => {
+    //     let prescriptions = []
+    //     snapshot.docs.forEach((doc) => {
+    //         prescriptions.push({ ...doc.data(), id: doc.id })
+    //     })
+    //     setState(prev => ({
+    //         ...prev,
+    //         prescriptionData: prescriptions
+    //     }));
+    // });
+
     useEffect(() => {
-        let prescriptions = []
-        getDocs(prescriptionRef)
-            .then((snapshot) => {
-                snapshot.docs.forEach((doc) => {
-                    prescriptions.push({ ...doc.data(), id: doc.id })
-                })
-                setState({
-                    prescriptionData: prescriptions
-                });
-            })
-            .catch(err => {
-                console.log(err.message)
+        // calls the onSnapshot function as well as stores the unsubscribe function into the unsub variable
+        const unsub = onSnapshot(prescriptionRef, (snapshot) => {
+            let prescriptions = [];
+            snapshot.docs.forEach((doc) => {
+                prescriptions.push({ ...doc.data(), id: doc.id })
             });
+            setState(prev => ({
+                ...prev,
+                prescriptionData: prescriptions
+            }));
+        });
+
+        // unsubscribe when the component unmounts to prevent memory leak
+        // otherwise the subscription would still be active even after component unmounts
+        return () => {
+            unsub();
+        }
     }, []);
 
-    const createPrescription = (prescription) => {
+    // subscribe to the record collection to get real time collection data
+    // onSnapshot(recordRef, (snapshot) => {
+    //     let records = []
+    //     snapshot.docs.forEach((doc) => {
+    //         records.push({ ...doc.data(), id: doc.id })
+    //     })
+    //     setState(prev => ({
+    //         ...prev,
+    //         recordData: records
+    //     }));
+    // });
+
+    // METHODS FOR PRESCRIPTION COLLECTION
+    const createPrescription = async (prescription) => {
         return addDoc(prescriptionRef, prescription)
             .then((docRef) => {
                 const res = {
                     ...prescription,
                     id: docRef.id
-                }
+                };
                 return res;
             })
             .catch((err) => {
@@ -88,8 +133,22 @@ export default function usePrescriptionData() {
             });
     };
 
-    // task to get record in record collection
-    const getRecord = (prescriptionId, doseId) => {
+    const updatePrescription = (id, prescription) => {
+        const docRef = doc(db, 'prescription', id);
+        updateDoc(docRef, {
+            ...prescription
+        });
+    };
+
+    const deletePrescription = async (id) => {
+        const docRef = doc(db, 'prescription', id);
+        deleteDoc(docRef);
+    };
+
+
+    // METHODS FOR RECORD COLLECTION
+
+    const getRecord = async (prescriptionId, doseId) => {
         let records = []
         const q = query(recordRef, where("prescriptionId", "==", prescriptionId), where("doseId", "==", parseInt(doseId)));
 
@@ -97,23 +156,21 @@ export default function usePrescriptionData() {
             .then((snapshot) => {
                 snapshot.docs.forEach((doc) => {
                     records.push({ ...doc.data(), id: doc.id })
-                })
+                });
 
                 if (records.length > 1) {
                     throw new Error(`Error! More than one record found with prescriptionId: ${prescriptionId} and doseId: ${doseId}`);
                 } else if (records.length == 0) {
                     return null
-                }
-                return records[0]
+                };
+                return records[0];
             })
             .catch(err => {
                 console.log(err.message)
             });
     };
 
-
-    // task to create records in record collection
-    const createRecord = (record) => {
+    const createRecord = async (record) => {
         const recordToCreate = {
             prescriptionId: record.prescriptionId,
             doseId: parseInt(record.doseId),
@@ -136,17 +193,13 @@ export default function usePrescriptionData() {
 
     // takes 2 arguments: document reference and object
     // it only updates document based on object, it will leave other properties alone if not in the update object
-    // updateDoc(docRef, {
-    //     title: 'updated title'
-    // })
-    // update an existing record
     const updateRecord = (id, taken, takenAt) => {
         const docRef = doc(db, 'record', id);
         updateDoc(docRef, {
             taken: taken,
             takenAt: taken ? serverTimestamp() : takenAt
-        })
-    }
+        });
+    };
 
-    return { state, createPrescription, getRecord, createRecord, updateRecord };
+    return { state, createPrescription, updatePrescription, deletePrescription, getRecord, createRecord, updateRecord };
 };
