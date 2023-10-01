4e08c1889734f365db7e0d9b509de8d7c184a48e
jameshuang98 <jameshuang337@gmail.com>
2023-09-30 21:29:26 -0700

    check recurringDays for today's prescriptions

--- client/app/tabs/home/Home.jsx
+++ client/app/tabs/home/Home.jsx
@@ -3,36 +3,45 @@ import React from 'react'
 import styles from "./home.style";
 import PrescriptionTimeSlot from '../../../components/home/PrescriptionTimeSlot';
 import usePrescriptionData from '../../hooks/usePrescriptionData';
+import { getSelectedDays } from '../../helpers/helpers';
+import { daysOfWeek } from '../../../constants/models';
 
 const Home = () => {
   const { state } = usePrescriptionData();
   // console.log("Home.jsx state", state);
   let timeslotData = {};
   for (const p of state.prescriptionData) {
-    let json = JSON.parse(p.json);
-    for (const doseId in json) {
-      const t = json[doseId]
-      if (!timeslotData[t]) {
-        timeslotData[t] = []
+    const recurringDays = getSelectedDays(p.frequency);
+    const today = new Date();
+    const todayIndex = today.getDay();
+    const days = Object.keys(daysOfWeek);
+
+    if (recurringDays.includes(days[todayIndex])) {
+      let json = JSON.parse(p.json);
+      for (const doseId in json) {
+        const t = json[doseId]
+        if (!timeslotData[t]) {
+          timeslotData[t] = []
+        }
+        timeslotData[t].push({
+          title: p.title,
+          dose: p.dose,
+          doseId: doseId,
+          prescriptionId: p.id
+        })
       }
-      timeslotData[t].push({
-        title: p.title,
-        dose: p.dose,
-        doseId: doseId,
-        prescriptionId: p.id
-      })
     }
   }
   // console.log("timeslotData", timeslotData)
   const timeslots = Object.keys(timeslotData);
 
-  const parsedTimeslots = timeslots.map((time, idx) =>
+  const parsedTimeslots = timeslots.length > 0 ? timeslots.map((time, idx) =>
     <PrescriptionTimeSlot
       key={idx}
       time={time}
       doseList={timeslotData[time]}
     />
-  );
+  ) : <Text>No prescriptions for today</Text>;
 
   return (
     <SafeAreaView style={styles.home}>
